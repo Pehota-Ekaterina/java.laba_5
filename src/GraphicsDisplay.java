6f00a064e5f8a22ec73489ffb6cedbfd4b94e31c
Pehota-Ekaterina <pexota02@list.ru>
2020-12-07 02:11:04 +0300

    add for 5 laba

--- src/GraphicsDisplay.java
+++ src/GraphicsDisplay.java
@@ -1,12 +1,54 @@
 import java.awt.*;
 import java.awt.font.FontRenderContext;
-import java.awt.geom.GeneralPath;
-import java.awt.geom.Line2D;
-import java.awt.geom.Point2D;
-import java.awt.geom.Rectangle2D;
+import java.awt.geom.*;
 import javax.swing.JPanel;
+import java.text.*;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.awt.event.MouseMotionListener;
+import java.util.EmptyStackException;
+import java.util.Stack;
 
 public class GraphicsDisplay extends JPanel {
+
+    class GraphPoint {
+        double xd;
+        double yd;
+        int x;
+        int y;
+        int n;
+    }
+
+    class Zone {
+        double MAXY;
+        double tmp;
+        double MINY;
+        double MAXX;
+        double MINX;
+        boolean use;
+    }
+
+    private Zone zone = new Zone();
+    private int[][] graphicsDataI;
+    private boolean transform = false;
+    private boolean showGrid = true;
+    private boolean PPP = false;
+    private boolean zoom=false;
+    private boolean selMode = false;
+    private boolean dragMode = false;
+    private double scaleX;
+    private double scaleY;
+    private DecimalFormat formatter = (DecimalFormat) NumberFormat.getInstance();
+    private BasicStroke gridStroke;
+    private BasicStroke hatchStroke;
+    private BasicStroke selStroke;
+    private Font captionFont;
+    private int mausePX = 0;
+    private int mausePY = 0;
+    private GraphPoint SMP;
+    double xmax;
+    private Rectangle2D.Double rect;
+    private Stack<Zone> stack = new Stack<Zone>();
 
     private Double[][] graphicsData;        // Список координат точек для построения графика
 
@@ -28,6 +70,7 @@ public class GraphicsDisplay extends JPanel {
 
     private Font axisFont;          // Различные шрифты отображения надписей
 
+
     public GraphicsDisplay() {
         setBackground(Color.WHITE);
         graphicsStroke = new BasicStroke(2.0f, BasicStroke.CAP_BUTT, BasicStroke.JOIN_ROUND, 10.0f, new float[]{1, 1, 1, 1, 1, 1, 3, 1, 2, 1, 2, 1}, 0.0f);
@@ -35,10 +78,21 @@ public class GraphicsDisplay extends JPanel {
         lineStroke = new BasicStroke(2.0f, BasicStroke.CAP_BUTT, BasicStroke.JOIN_MITER, 10.0f, null, 0.0f);
         markerStroke = new BasicStroke(1.0f, BasicStroke.CAP_BUTT, BasicStroke.JOIN_MITER, 10.0f, null, 0.0f);
         axisFont = new Font("Serif", Font.BOLD, 36);
+
+        gridStroke = new BasicStroke(1.0f, BasicStroke.CAP_BUTT, BasicStroke.JOIN_ROUND, 10.0f, null, 0.0f);
+        hatchStroke = new BasicStroke(1.0f, BasicStroke.CAP_BUTT, BasicStroke.JOIN_ROUND, 10.0f, null, 0.0f);
+        selStroke = new BasicStroke(1.0f, BasicStroke.CAP_BUTT, BasicStroke.JOIN_MITER, 10.0f, new float[] { 8, 8 }, 0.0f);
+        captionFont = new Font("Serif", Font.BOLD, 10);
+        MouseMotionHandler mouseMotionHandler = new MouseMotionHandler();
+        addMouseMotionListener(mouseMotionHandler);
+        addMouseListener(mouseMotionHandler);
+        rect = new Rectangle2D.Double();
+        zone.use = false;
     }
 
     public void showGraphics(Double[][] graphicsData) {
         this.graphicsData = graphicsData;
+        graphicsDataI = new int[graphicsData.length][2];
         repaint();      // Запросить перерисовку компонента, т.е. неявно вызвать paintComponent()
     }
 
@@ -70,6 +124,14 @@ public class GraphicsDisplay extends JPanel {
 // Еѐ верхний левый угол это (minX, maxY) - правый нижний это (maxX, minY)
         minX = graphicsData[0][0];
         maxX = graphicsData[graphicsData.length - 1][0];
+
+        if (zone.use) {// && zone.MINX>minX){
+            minX = zone.MINX;
+        }
+        if (zone.use) {// && zone.MAXX<maxX){
+            maxX = zone.MAXX;
+        }
+
         minY = graphicsData[0][1];
         maxY = minY;
 
@@ -80,38 +142,60 @@ public class GraphicsDisplay extends JPanel {
             }
             if (graphicsData[i][1] > maxY) {
                 maxY = graphicsData[i][1];
+                xmax = graphicsData[i][1];
             }
         }
 
+        if (zone.use) {// && zone.MINY>minY){
+            minY = zone.MINY;
+        }
+        if (zone.use) {// && zone.MAXY<maxY){
+            maxY = zone.MAXY;
+        }
+
 // Шаг 4 - Определить (исходя из размеров окна) масштабы по осям X и Y - сколько пикселов приходится на единицу длины по X и по Y
-        double scaleX = getSize().getWidth() / (maxX - minX);
-        double scaleY = getSize().getHeight() / (maxY - minY);
+        scaleX = 1.0 / (maxX - minX);
+        scaleY = 1.0 / (maxY - minY);
+
+        if (!transform)
+            scaleX *= getSize().getWidth();
+        else
+            scaleX *= getSize().getHeight();
+        if (!transform)
+            scaleY *= getSize().getHeight();
+        else
+            scaleY *= getSize().getWidth();
+        if (transform) {
+            ((Graphics2D) g).rotate(-Math.PI / 2);
+            ((Graphics2D) g).translate(-getHeight(), 0);
+        }
 
 // Шаг 5 - выбор масштаба (минимальный)
         scale = Math.min(scaleX, scaleY);
 
 // Шаг 6 - корректировка границ отображаемой области согласно выбранному масштабу
-        if (scale == scaleX) {
-/* Если за основу был взят масштаб по оси X, значит по оси Y
-делений меньше,
-* т.е. подлежащий визуализации диапазон по Y будет меньше
-высоты окна.
-* Значит необходимо добавить делений, сделаем это так:
-* 1) Вычислим, сколько делений влезет по Y при выбранном масштабе - getSize().getHeight()/scale
-* 2) Вычтем из этого сколько делений требовалось изначально
-* 3) Набросим по половине недостающего расстояния на maxY и
-minY
-*/
-            double yIncrement = (getSize().getHeight() / scale - (maxY - minY)) / 2;
-            maxY += yIncrement;
-            minY -= yIncrement;
-        }
-
-        if (scale == scaleY) {
-// Если за основу был взят масштаб по оси Y, действовать по аналогии
-            double xIncrement = (getSize().getWidth() / scale - (maxX - minX)) / 2;
-            maxX += xIncrement;
-            minX -= xIncrement;
+        if(!zoom){
+            if (scale == scaleX) {
+                double yIncrement = 0;
+                if (!transform)
+                    yIncrement = (getSize().getHeight() / scale - (maxY - minY)) / 2;
+                else
+                    yIncrement = (getSize().getWidth() / scale - (maxY - minY)) / 2;
+                maxY += yIncrement;
+                minY -= yIncrement;
+            }
+            if (scale == scaleY) {
+                double xIncrement = 0;
+                if (!transform) {
+                    xIncrement = (getSize().getWidth() / scale - (maxX - minX)) / 2;
+                    maxX += xIncrement;
+                    minX -= xIncrement;
+                } else {
+                    xIncrement = (getSize().getHeight() / scale - (maxX - minX)) / 2;
+                    maxX += xIncrement;
+                    minX -= xIncrement;
+                }
+            }
         }
 
 // Шаг 7 - Сохранить текущие настройки холста
@@ -126,12 +210,50 @@ minY
         paintGraphics(canvas);
         if (showLines) paintLines(canvas);
         if (showMarkers) paintMarkers(canvas);
+        if (SMP != null)
+            paintHint(canvas);
+        if (selMode) {
+            canvas.setColor(Color.GREEN);               //ПУНКТИРНАЯ ЛИНИЯ ВЫДЕЛЕНИЯ ОБЛАСТИ
+            canvas.setStroke(selStroke);
+            canvas.draw(rect);
+        }
 
 // Шаг 9 - Восстановить старые настройки холста
         canvas.setFont(oldFont);
         canvas.setPaint(oldPaint);
         canvas.setColor(oldColor);
         canvas.setStroke(oldStroke);
+    }
+
+    protected void paintHint(Graphics2D canvas) {
+        Color oldColor = canvas.getColor();
+        canvas.setColor(Color.GREEN);
+        StringBuffer label = new StringBuffer();
+        label.append("(x = ");
+        label.append(formatter.format((SMP.xd)));
+        label.append("; y = ");
+        label.append(formatter.format((SMP.yd)));
+        label.append(")");
+        FontRenderContext context = canvas.getFontRenderContext();
+        Rectangle2D bounds = captionFont.getStringBounds(label.toString(),context);
+        if (!transform) {
+            int dy = -10;
+            int dx = +7;
+            if (SMP.y < bounds.getHeight())
+                dy = +13;
+            if (getWidth() < bounds.getWidth() + SMP.x + 20)
+                dx = -(int) bounds.getWidth() - 15;
+            canvas.drawString (label.toString(), SMP.x + dx, SMP.y + dy);
+        } else {
+            int dy = 10;
+            int dx = -7;
+            if (SMP.x < 10)
+                dx = +13;
+            if (SMP.y < bounds.getWidth() + 20)
+                dy = -(int) bounds.getWidth() - 15;
+            canvas.drawString (label.toString(), getHeight() - SMP.y + dy, SMP.x + dx);
+        }
+        canvas.setColor(oldColor);
     }
 
     protected void paintGraphics(Graphics2D canvas) {
@@ -142,6 +264,14 @@ minY
         for (int i = 0; i < graphicsData.length; i++) {
 // Преобразовать значения (x,y) в точку на экране point
             Point2D.Double point = xyToPoint(graphicsData[i][0], graphicsData[i][1]);
+
+            graphicsDataI[i][0] = (int) point.getX();
+            graphicsDataI[i][1] = (int) point.getY();
+            if (transform) {
+                graphicsDataI[i][0] = (int) point.getY();
+                graphicsDataI[i][1] = getHeight() - (int) point.getX();
+            }
+
             if (i > 0) {
                 graphics.lineTo(point.getX(), point.getY());        // Не первая итерация цикла - вести линию в точку point
             } else {
@@ -159,7 +289,6 @@ minY
             cr_sum += point[1];
         }
         cr_sum /= graphicsData.length;
-        System.out.println(graphicsData.length);
 
         for (Double[] point : graphicsData) {
 
@@ -290,7 +419,29 @@ minY
         double deltaX = x - minX;
 // Вычисляем смещение Y от точки верхней точки (maxY)
         double deltaY = maxY - y;
-        return new Point2D.Double(deltaX * scale, deltaY * scale);
+
+        if(!zoom)
+            return new Point2D.Double(deltaX * scale, deltaY * scale);
+        else
+            return new Point2D.Double(deltaX * scaleX, deltaY * scaleY);
+    }
+
+    protected Point2D.Double pointToXY(int x, int y) {
+        Point2D.Double p = new Point2D.Double();
+        if (!transform) {
+            p.x = x / scale + minX;
+            int q = (int) xyToPoint(0, 0).y;
+            p.y = maxY - maxY * ((double) y / (double) q);
+        } else {
+            if(!zoom){
+                p.y = -x / scale + (maxY);
+                p.x = -y / scale + maxX;
+            }else{
+                p.y = -x / scaleY + (maxY);
+                p.x = -y / scaleX + maxX;
+            }
+        }
+        return p;
     }
 
     /* Метод-помощник, возвращающий экземпляр класса Point2D.Double
@@ -302,5 +453,168 @@ minY
 // Задать еѐ координаты как координаты существующей точки + заданные смещения
         dest.setLocation(src.getX() + deltaX, src.getY() + deltaY);
         return dest;
+    }
+
+    public class MouseMotionHandler implements MouseMotionListener, MouseListener {
+        private double comparePoint(Point p1, Point p2) {
+            return Math.sqrt(Math.pow(p1.x - p2.x, 2) + Math.pow(p1.y - p2.y, 2));
+        }
+
+        private GraphPoint find(int x, int y) {
+            GraphPoint smp = new GraphPoint();
+            GraphPoint smp2 = new GraphPoint();
+            double r, r2 = 1000;
+            for (int i = 0; i < graphicsData.length; i++) {
+                Point p = new Point();
+                p.x = x;
+                p.y = y;
+                Point p2 = new Point();
+                p2.x = graphicsDataI[i][0];
+                p2.y = graphicsDataI[i][1];
+                r = comparePoint(p, p2);
+                if (r < 7.0) {
+                    smp.x = graphicsDataI[i][0];
+                    smp.y = graphicsDataI[i][1];
+                    smp.xd = graphicsData[i][0];
+                    smp.yd = graphicsData[i][1];
+                    smp.n = i;
+                    if (r < r2) {
+                        r2 = r;
+                        smp2 = smp;
+                    }
+                    return smp2;
+                }
+            }
+            return null;
+        }
+
+        public void mouseMoved(MouseEvent ev) {    // показывает координаты точек
+            GraphPoint smp;
+            smp = find(ev.getX(), ev.getY());
+           // setCursor(Cursor.getPredefinedCursor(0));
+            if (smp != null) {
+                SMP = smp;
+            } else {
+                SMP = null;
+            }
+            repaint();
+        }
+
+        public void mouseDragged(MouseEvent e) {
+            if (selMode) {
+                if (!transform)
+                    rect.setFrame(mausePX, mausePY, e.getX() - rect.getX(),
+                            e.getY() - rect.getY());
+                else {
+                    rect.setFrame(-mausePY + getHeight(), mausePX, -e.getY()
+                            + mausePY, e.getX() - mausePX);
+                }
+                repaint();
+            }
+            if (dragMode) {
+                if (!transform) {
+                    if(pointToXY(e.getX(), e.getY()).y<maxY && pointToXY(e.getX(), e.getY()).y>minY){
+                        graphicsData[SMP.n][1] = pointToXY(e.getX(), e.getY()).y;
+                        SMP.yd = pointToXY(e.getX(), e.getY()).y;
+                        SMP.y = e.getY();
+                    }
+                } else {
+                    if(pointToXY(e.getX(), e.getY()).y<maxY && pointToXY(e.getX(), e.getY()).y>minY){
+                        graphicsData[SMP.n][1] = pointToXY(e.getX(), e.getY()).y;
+                        SMP.yd = pointToXY(e.getX(), e.getY()).y;
+                        SMP.x = e.getX();
+                    }
+                }
+                repaint();
+            }
+        }
+
+        public void mouseClicked(MouseEvent e) {        //возрат к обычному масштабу
+            if (e.getButton() != 3)
+                return;
+
+            try {
+                zone = stack.pop();
+            } catch (EmptyStackException err) {
+
+            }
+
+            if(stack.empty())
+                zoom=false;
+            repaint();
+        }
+
+        public void mouseEntered(MouseEvent arg0) {
+
+        }
+
+        public void mouseExited(MouseEvent arg0) {
+
+        }
+
+        public void mousePressed(MouseEvent e) {                //показывает область выдиления
+                selMode = true;
+                mausePX = e.getX();
+                mausePY = e.getY();
+                if (!transform)
+                    rect.setFrame(e.getX(), e.getY(), 0, 0);
+                else
+                    rect.setFrame(e.getX(), e.getY(), 0, 0);
+            //}
+        }
+
+        public void mouseReleased(MouseEvent e) {             //увеличение масштаба
+            rect.setFrame(0, 0, 0, 0);
+            if (e.getButton() != 1) {
+                repaint();
+                return;
+            }
+            if (selMode) {
+                if (!transform) {
+                    if (e.getX() <= mausePX || e.getY() <= mausePY)
+                        return;
+                    int eY = e.getY();
+                    int eX = e.getX();
+                    if (eY > getHeight())
+                        eY = getHeight();
+                    if (eX > getWidth())
+                        eX = getWidth();
+                    double MAXX = pointToXY(eX, 0).x;
+                    double MINX = pointToXY(mausePX, 0).x;
+                    double MAXY = pointToXY(0, mausePY).y;
+                    double MINY = pointToXY(0, eY).y;
+                    stack.push(zone);
+                    zone = new Zone();
+                    zone.use = true;
+                    zone.MAXX = MAXX;
+                    zone.MINX = MINX;
+                    zone.MINY = MINY;
+                    zone.MAXY = MAXY;
+                    selMode = false;
+                    zoom=true;
+                } else {
+                    if (pointToXY(mausePX, 0).y <= pointToXY(e.getX(), 0).y
+                            || pointToXY(0, e.getY()).x <= pointToXY(0, mausePY).x)
+                        return;
+                    int eY = e.getY();
+                    int eX = e.getX();
+                    if (eY < 0)
+                        eY = 0;
+                    if (eX > getWidth())
+                        eX = getWidth();
+                    stack.push(zone);
+                    zone = new Zone();
+                    zone.use = true;
+                    zone.MAXY = pointToXY(mausePX, 0).y;
+                    zone.MAXX = pointToXY(0, eY).x;
+                    zone.MINX = pointToXY(0, mausePY).x;
+                    zone.MINY = pointToXY(eX, 0).y;
+                    selMode = false;
+                    zoom=true;
+                }
+
+            }
+            repaint();
+        }
     }
 }
